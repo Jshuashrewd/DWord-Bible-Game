b831e26163ae9094cf6416fc87c9d1611c1ecb07
Jshuashrewd <joshadeyanju23@gmail.com>
2024-11-16 08:20:00 +0100

    added data

---
+++ scripts/data.js
@@ -0,0 +1,55 @@
+const bibleQiuzArray = [{
+  id: "0",
+  question: "What was Jesus' first recorded miracle in the Bible",
+  options: [
+    "Healing a blind man",
+    "Turning water into wine",
+    "Raising Lazarus"
+  ],
+  correct: "Turning water into wine",
+  hint: "John 2:1-11",
+},
+{
+  id: "1",
+  question: "Who did Jesus heal with a touch of his cloak",
+  options: [
+    "A leper",
+    "woman with bleeding",
+    "A paralyzed man"
+  ],
+  correct: "woman with bleeding",
+  hint: "Matthew 9:20-22",
+},
+{
+  id: "2",
+  question: "What miracle did Jesus perform for 5,000 people",
+  options: [
+    "",
+    "",
+    "",
+  ],
+  correct: "",
+  hint: "",
+},
+{
+  id: "3",
+  question: "",
+  options: [
+    "",
+    "",
+    "",
+  ],
+  correct: "",
+  hint: "",
+},
+{
+  id: "4",
+  question: "",
+  options: [
+    "",
+    "",
+    "",
+  ],
+  correct: "",
+  hint: "",
+}]
